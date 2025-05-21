3c527f5a66f5d9a0b53bfdaba10ee7264c1fc1b5
aaevans03 <aaevans03@gmail.com>
2025-05-21 17:09:25 -0600

    Implemented UserServiceTests.loginUser and UserServiceTests.loginUserBadPassword

--- server/src/test/java/service/UserServiceTests.java
+++ server/src/test/java/service/UserServiceTests.java
@@ -9,8 +9,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import server.exceptions.AlreadyTakenException;
+import server.exceptions.InvalidCredentialsException;
+import service.request.LoginRequest;
 import service.request.RegisterRequest;
-import service.result.RegisterResult;
 
 import java.util.HashMap;
 
@@ -30,8 +31,9 @@ class UserServiceTests {
     }
 
     @Test
+    // successfully register a new user
     void registerNewUser() {
-        // successfully register a new user
+        // expected output
         UserData user = new UserData("bob", "password", "e@mail.com");
         var expected = new HashMap<String, UserData>();
         expected.put("bob", user);
@@ -63,12 +65,34 @@ class UserServiceTests {
     }
 
     @Test
+    // successfully login a user
     void loginUser() {
-        // successfully login a user
+        // expected output
+        UserData user = new UserData("bob", "password", "e@mail.com");
+        userDB.createUser(user);
+
+        // use the service to login
+        var userService = new UserService();
+        var loginResult = userService.login(new LoginRequest("bob", "password"));
+
+        // check and see if the expected AuthData matches the LoginResult
+        var expectedAuthData = authDB.getMap().values().toArray(new AuthData[0]);
+        Assertions.assertEquals(1, expectedAuthData.length);
+        Assertions.assertEquals(expectedAuthData[0].authToken(), loginResult.authToken());
+        Assertions.assertEquals(expectedAuthData[0].username(), loginResult.username());
     }
 
+    @Test
+    // try to log in a user with a bad password
     void loginUserBadPassword() {
-        // try to log in a user with a bad password
+        // create user
+        UserData user = new UserData("bob", "password", "e@mail.com");
+        userDB.createUser(user);
+
+        // use the service to login
+        var userService = new UserService();
+        var loginRequest = new LoginRequest("bob", "wrongPassword");
+        Assertions.assertThrows(InvalidCredentialsException.class, () -> userService.login(loginRequest));
     }
 
     @Test
