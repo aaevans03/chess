a896e193f2e905049674ea89ccade7d48cf9bdcd
aaevans03 <aaevans03@gmail.com>
2025-05-27 21:22:45 -0600

    Implemented createUser and createExistingUser tests in MySqlUserDAOTests

--- server/src/test/java/dataaccess/MySqlUserDAOTests.java
+++ server/src/test/java/dataaccess/MySqlUserDAOTests.java
@@ -5,6 +5,7 @@ import model.UserData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mindrot.jbcrypt.BCrypt;
 
 import java.sql.SQLException;
 
@@ -15,6 +16,7 @@ class MySqlUserDAOTests {
     @BeforeEach
     void setUp() throws DataAccessException {
         userDB = new MySqlUserDAO();
+        userDB.clearUserData();
     }
 
     @Test
@@ -34,18 +36,7 @@ class MySqlUserDAOTests {
             Assertions.assertNull(userDB.getUser("user4"));
             Assertions.assertNull(userDB.getUser("user5"));
 
-            int rowCount = 1;
-
-            try (var conn = DatabaseManager.getConnection()) {
-                try (var preparedStatement = conn.prepareStatement("SELECT COUNT(username) FROM userData")) {
-                    try (var resultSet = preparedStatement.executeQuery()) {
-                        resultSet.next();
-                        rowCount = resultSet.getInt(1);
-                    }
-                }
-            }
-
-            Assertions.assertEquals(0, rowCount);
+            Assertions.assertEquals(0, countTableEntries());
 
         } catch (DataAccessException | SQLException e) {
             System.out.println("Exception occurred: " + e.getMessage());
@@ -54,9 +45,51 @@ class MySqlUserDAOTests {
 
     @Test
     void createUser() {
+        try {
+            var userData = new UserData("jill", "mySecretPassword", "jill@mail.com");
+            userDB.createUser(userData);
+
+            var retrievedUserData = userDB.getUser("jill");
+
+            Assertions.assertEquals(userData.username(), retrievedUserData.username());
+            Assertions.assertTrue(BCrypt.checkpw("mySecretPassword", retrievedUserData.password()));
+            Assertions.assertEquals(userData.email(), retrievedUserData.email());
+            Assertions.assertEquals(1, countTableEntries());
+
+        } catch (DataAccessException | SQLException e) {
+            System.out.println("Exception occurred: " + e.getMessage());
+        }
+    }
+
+    @Test
+    void createExistingUser() {
+        Assertions.assertThrows(DataAccessException.class, () -> {
+            var userData = new UserData("aaron", "securePASSWORD", "aaron@mail.net");
+            userDB.createUser(userData);
+            userDB.createUser(userData);
+        });
     }
 
     @Test
     void getUser() {
+
+    }
+
+    @Test
+    void getNonExistentUser() {
+
+    }
+
+    private int countTableEntries() throws SQLException, DataAccessException {
+        int rowCount;
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var preparedStatement = conn.prepareStatement("SELECT COUNT(username) FROM userData")) {
+                try (var resultSet = preparedStatement.executeQuery()) {
+                    resultSet.next();
+                    rowCount = resultSet.getInt(1);
+                }
+            }
+        }
+        return rowCount;
     }
 }
