d40d826bb3a56f1c87677304e64d80ef7bf56972
aaevans03 <aaevans03@gmail.com>
2025-05-28 22:03:30 -0600

    Fixed duplicate code in DataAccessException

--- server/src/main/java/dataaccess/DataAccessException.java
+++ server/src/main/java/dataaccess/DataAccessException.java
@@ -1,28 +1,16 @@
 package dataaccess;
 
-import server.ObjectEncoderDecoder;
-import spark.Request;
-import spark.Response;
-
-import java.util.Map;
+import server.exceptions.ServerException;
 
 /**
  * Indicates there was an error connecting to the database
  */
-public class DataAccessException extends Exception {
+public class DataAccessException extends ServerException {
     public DataAccessException(String message) {
         super(message);
     }
 
     public DataAccessException(String message, Throwable ex) {
-        super(message, ex);
-    }
-
-    public static void errorHandler(Exception e, Request ignoredReq, Response res) {
-        var objectEncoderDecoder = new ObjectEncoderDecoder();
-        var body = objectEncoderDecoder.encode(Map.of("message", String.format("Error: %s", e.getMessage())));
-        res.type("application/json");
-        res.status(500);
-        res.body(body);
+        super(message);
     }
 }
