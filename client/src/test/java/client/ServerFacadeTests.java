8ee8a5ba476c1fc0f4cc2e26a2c562e96bb55b53
aaevans03 <aaevans03@gmail.com>
2025-06-03 13:46:26 -0600

    Created list of unit tests for ServerFacade

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,18 +1,24 @@
 package client;
 
+import dataaccess.mysql.MySqlAuthDAO;
+import dataaccess.mysql.MySqlGameDAO;
+import dataaccess.mysql.MySqlUserDAO;
 import org.junit.jupiter.api.*;
 import server.Server;
+import server.ServerFacade;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade("http://localhost:" + port);
     }
 
     @AfterAll
@@ -20,10 +26,75 @@ public class ServerFacadeTests {
         server.stop();
     }
 
+    @BeforeEach
+    void beforeEach() {
+        new MySqlGameDAO().clearGameData();
+        new MySqlAuthDAO().clearAuthData();
+        new MySqlUserDAO().clearUserData();
+    }
 
     @Test
     public void sampleTest() {
         Assertions.assertTrue(true);
     }
 
+    @Test
+    void register() {
+
+    }
+
+    @Test
+    void registerFail() {
+
+    }
+
+    @Test
+    void login() {
+
+    }
+
+    @Test
+    void loginFail() {
+
+    }
+
+    @Test
+    void logout() {
+
+    }
+
+    @Test
+    void logoutFail() {
+
+    }
+
+    @Test
+    void listGames() {
+
+    }
+
+    @Test
+    void listGamesFail() {
+
+    }
+
+    @Test
+    void create() {
+
+    }
+
+    @Test
+    void createFail() {
+
+    }
+
+    @Test
+    void join() {
+
+    }
+
+    @Test
+    void joinFail() {
+        
+    }
 }
